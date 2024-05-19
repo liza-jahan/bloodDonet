ec08e2143d94614de3b054ec5cebb5ea353bc112
Eakteker jahan Liza <74711092+liza-jahan@users.noreply.github.com>
2024-05-20 02:42:11 +0600

    Create project

---
+++ src/main/java/com/example/blooddonet/BloodDonetApplication.java
@@ -0,0 +1,13 @@
+package com.example.blooddonet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodDonetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloodDonetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/blooddonet/BloodDonetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blooddonet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
