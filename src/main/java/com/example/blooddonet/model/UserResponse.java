f07689c53b4802fb0e60f0fc99543796cbe3a7f9
Eakteker jahan Liza <74711092+liza-jahan@users.noreply.github.com>
2024-05-21 14:24:19 +0600

    User class create

--- src/main/java/com/example/blooddonet/model/UserResponse.java
+++ src/main/java/com/example/blooddonet/model/UserResponse.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,10 +21,15 @@ public class UserResponse {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Nonnull
     private Long id;
+    @NotBlank(message = "First name field is required")
     private String firstName;
+    @NotBlank(message = "Last name field is required")
     private String lastName;
+    @NotBlank(message = "Phone number field is required")
     private  String phoneNumber;
+    @NotBlank(message = "BloodGroup field is required")
     private  String bloodGroup;
+
     private  String permanentAddress;
 
 }
